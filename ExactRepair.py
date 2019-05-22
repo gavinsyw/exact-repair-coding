effb15d92cac962a32bd078852d1874aaac0883c
gavinsyw <gavinsyw@163.com>
2019-05-22 18:22:06 +0800

    生成Shannon不等式

--- ExactRepair.py
+++ ExactRepair.py
@@ -13,7 +13,25 @@ class ExactRepair:
         print("Initializing ...")
         self.terms = reduction.termTable()
         print("Reduction Complete, item number: ", len(self.terms))
+        self.inequalities = []
+        self.equalities = []
+        self.termNum = len(self.terms)
 
+    def HGreaterThan0(self): # Shannon Inequality for H>0
+        inequalities = []
+        standardVec = [0 for i in range(self.termNum+1)]
+        for i in range(self.termNum):
+            standardVec[i] = -1
+            inequalities.append(standardVec.copy())
+            standardVec[i] = 0
+        return inequalities
+
+    def IGreaterThan0(self): # Shannon Inequality for I>0
+        inequalities = []
+        standardVec = [0 for i in range(self.termNum)]
+        
+        return None
 
 if __name__ == "__main__":
     e = ExactRepair(4)
+    e.HGreaterThan0()
